0ed81b6a2d4638978ba53acf19b2bfb7ba2112bb
Gaoussou Doucoure <gaousd4@gmail.com>
2025-06-09 23:32:21 -0700

    Query to get all sales information for a specific dealer for a specific date range

--- salesInfoForDealership.sql
+++ salesInfoForDealership.sql
@@ -1,5 +1,4 @@
 -- 6. Get all sales information for a specific dealer for a specific date range:
-SELECT *
-FROM sales
-WHERE dealer_id = 'specific_dealer_id'
-AND sale_date BETWEEN 'start_date' AND 'end_date';
+SELECT * FROM cardealership.sales_contracts
+where `when` between '2025-06-02' AND '2025-06-06'
+AND dealership_id = 4
